ea47ab9e5d3630743a915d26d1caf2fe19dc5726
colinjlacy <colin@webcake.co>
2015-06-03 18:03:51 -0500

    [create-client] creates a user model in Devise, adds an admin column

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,6 @@
 class Invoice < ActiveRecord::Base
+	has_one :user
+	validates_associated :user
 	validates :title, presence: true
 	validates :due_date, presence: true
 	validates :charge_amount, presence: true, numericality: {only_integer: true}
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  root 'invoices#index'
   resources :invoices
 
   # The priority is based upon order of creation: first created -> highest priority.
