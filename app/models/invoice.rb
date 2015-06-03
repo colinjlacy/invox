bce31e47cfe659a512a14340ebee5f3b8282f7c5
colinjlacy <colin@webcake.co>
2015-06-02 20:33:07 -0500

    [invoice-validations] adds integer validation to charge amount

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,5 @@
 class Invoice < ActiveRecord::Base
 	validates :title, presence: true
 	validates :due_date, presence: true
-	validates :charge_amount, presence: true
+	validates :charge_amount, presence: true, numericality: {only_integer: true}
 end
